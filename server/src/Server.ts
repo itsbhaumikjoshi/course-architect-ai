8f0744c247cffdd2e6f3d5baa846b8372a59d5cf
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2025-07-24 18:34:45 +0530

    integrated routes

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,7 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 import { AccessEnv, Logger } from "@/helpers";
 import { AppDataSource } from "@/helpers";
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import Router from "@/routes";
 
 export default class Server {
 
@@ -12,30 +13,38 @@ export default class Server {
 
     private async init(): Promise<void> {
         try {
+            // establish db connection
             const dataSource = AppDataSource.getDataSource();
             await dataSource.initialize();
             const host = (dataSource.options as PostgresConnectionOptions).host;
             const name = dataSource.options.database;
             this.logger.info(`Connected at ${host} to ${name}.`);
-        } catch(e) {
-            this.logger.error("Failed to establish connection with DB");
-            process.exit(1);
+        } catch (e) {
+            this.logger.error("Failed to establish connection with DB"); // in future use circuit breaker with expo time retry
         }
     }
 
     async start() {
         try {
             await this.init();
+            
+            // parse string body
+            this.app.use(express.json());
+            this.app.use(express.urlencoded({ extended: true }));
+            
+            //routes
+            const router = new Router();
+            this.app.use("/", router.get());
 
             // if applicable route not found then send 404 response
-            this.app.use((req, res) => {
+            this.app.use((_, res) => {
                 res.status(404).json({
                     message: "Route not found!"
                 });
             });
 
             // error handler if any encountered
-            this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+            this.app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
                 res.locals.message = err.message;
                 res.locals.error = req.app.get("env") === "development" ? err : {};
                 this.logger.error(err.message, err);
