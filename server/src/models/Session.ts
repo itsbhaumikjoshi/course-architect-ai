b8ebc925321f0bc4af0ae4cae4ba77776bd97229
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2025-07-31 02:14:41 +0530

    fixed validity check and added relation feild

--- server/src/models/Session.ts
+++ server/src/models/Session.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, RelationId, UpdateDateColumn } from "typeorm";
 import { User } from "@/models";
 
 @Entity("sessions")
@@ -7,9 +7,12 @@ export default class Session {
     @PrimaryGeneratedColumn("uuid")
     public id!: string;
 
-    @ManyToOne(() => User, (user) => user.sessions, { onDelete: "CASCADE" })
+    @ManyToOne(() => User, (user) => user.sessions, { onDelete: "CASCADE", nullable: false })
     @JoinColumn({ name: "user_id" })
     public user!: User | string;
+
+    @RelationId((session: Session) => session.user)
+    public userId!: string;
 
     @Column({ type: "timestamptz", name: "expires_at" })
     public expiresAt!: Date;
@@ -21,7 +24,7 @@ export default class Session {
     public deletedAt?: Date;
 
     isValid(): boolean {
-        return this.deletedAt == null && this.expiresAt < new Date();
+        return this.deletedAt == null && this.expiresAt > new Date();
     }
 
     // if token expxires in one day or less renew it.
