5e2286ce5c43dbaad1cded549a8a25dc2d747806
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2025-07-31 01:55:57 +0530

    added the entry point

---
+++ server/src/repository/index.ts
@@ -0,0 +1,5 @@
+export { default as CourseRepo } from "@/repository/CourseRepo";
+export { default as ModuleRepo } from "@/repository/ModuleRepo";
+export { default as LessonRepo } from "@/repository/LessonRepo";
+export { default as SessionRepo } from "@/repository/SessionRepo";
+export { default as UserRepo } from "@/repository/UserRepo";
