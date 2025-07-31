83f825f2c63691d4921909207cdfcdbfb76415fc
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2025-08-01 01:06:29 +0530

    created basic next app with material ui

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+import theme from '@/theme';
+
+const roboto = Roboto({
+  weight: ['300','400','500','700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
