51f3353aa8bd564969963418664504fd0ae52260
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2025-08-06 19:00:12 +0530

    renamed types as per ts stands

--- server/src/types/course.ts
+++ server/src/types/course.ts
@@ -1,48 +1,80 @@
-export type LLMResponse = {
+export type ILLMResponse = {
     title: string,
     description: string,
-    modules: Module[],
+    modules: IModule[],
 };
 
-export type Module = {
+export type IModule = {
+    id: number,
     title: string,
-    lessons: Lesson[]
+    lessons: ILesson[]
 };
 
-export type Lesson = {
+export type ILesson = {
     type: string,
     text?: string,
     url?: string,
     code?: string,
-    mcq?: MCQ[],
+    mcq?: IMCQ[],
 };
 
-export type MCQ = {
+export type IMCQ = {
     question: string,
     option: string[],
     answer: string
 };
 
-export type CreateCompleteCourse = {
+export type ICreateCourseFromLLMResponse = {
     userId: string,
     input: string
 };
 
-export type DeleteCourse = {
+export type IDeleteCourse = {
     userId: string,
     courseId: string
 };
 
 
-export type CreateCourse = {
+export type ICreateCourse = {
     title: string,
     description: string,
     userId: string,
 };
 
-export type CourseResponse = {
+export type ICourse = {
     id: string,
     created_at: Date,
     title: string,
     description: string,
+};
+
+export type IFindCourseByModuleId = {
+    id: string,
+    moduleId?: string,
+    userId: string
+};
+
+export type ICourseModuleResponse = {
+    title: string,
+    
+};
+
+export type IFindCourseById = {
+    id: string,
+    userId: string;
+};
+
+export type ICreateModule = {
+    id: number,
+    title: string,
+    courseId: string,
+};
+
+export type GetModule = {
+    id: string,
+}
+
+export type ICreateLesson = {
+    content: string,
+    moduleId: string,
 };
