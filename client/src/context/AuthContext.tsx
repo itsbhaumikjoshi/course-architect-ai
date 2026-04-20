c448f432c6acee541cf8704b104666c7e40ea601
Bhaumik Joshi <imbhaumikjoshiforyou@gmail.com>
2026-04-21 02:03:07 +0530

    fixed file path import

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { fetchUserData } from '../adapters/backend';
+import { fetchUserData } from '../adapters';
 import type { User } from '../adapters/types';
 
 interface AuthContextType {
